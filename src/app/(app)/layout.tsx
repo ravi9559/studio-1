514486712e98e26a693f3a2bd1596ea9946a2b24
Your Name <balaji@lakshmibalajio2o.com>
2025-06-27 08:03:00 +0000

    good amount of gap/space between the side panel and the module, fix this

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,17 +1,9 @@
-import { AppSidebar } from "@/components/layout/app-sidebar";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import React from 'react';
 
 export default function AppLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        {children}
-      </SidebarInset>
-    </SidebarProvider>
-  );
+  return <>{children}</>;
 }
